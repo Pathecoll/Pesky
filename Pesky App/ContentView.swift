56a2a237c65105b05c30b8c927e429ac2cc2beee
Hector Plata <hector.f.plata.s@me.com>
2021-01-14 11:29:32 -0500

    Initial Commit

---
+++ Pesky App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pesky App
+//
+//  Created by Hector Fabian Plata Santos on 14/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pesky App/Pesky_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pesky_AppApp.swift
+//  Pesky App
+//
+//  Created by Hector Fabian Plata Santos on 14/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Pesky_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
